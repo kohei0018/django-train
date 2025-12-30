4034441e7bd46d4876596d0f8ad8c8f9907a706a
kohei0018 <elevenonemind018@gmail.com>
2025-12-30 14:44:26 +0900

    migrate MariaDB

--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "myapp",
+        "USER": "myappuser",
+        "PASSWORD": "StrongPasswordHere!",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {"charset": "utf8mb4"},
     }
 }
 
